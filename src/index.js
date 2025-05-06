239a17fc837eaf1a585ccccf212bcc6fe6cb3f59
Jorge Munera <gerjo9211@hotmail.com>
2025-05-06 10:30:29 -0500

    eventos volatiles

--- src/index.js
+++ src/index.js
@@ -20,15 +20,8 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    // vamos a escuchar e evento cuando me emiten la posicion del circulo
     socket.on("circle-position", position => {
-        // ahora mi server deberia mandar esto a todos los usuarios onectados o clientes conectados
-        // io.emit("move-circle", position);
-
-        // pero en lugar de io.emit como lo tenemos arriba, lo mejor es usar broadcast
-        // broadcast emite el evento a todos menos a el socket especifico (a mi)
         socket.broadcast.emit("move-circle", position);
-
     })
 })
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -6,20 +6,19 @@ const drag = e => {
         left: e.clientX + "px"
     }
 
-    // y para no depender del servidor primero dibujamos el circulito y luego emitimos el evento con la position
     drawCircle(position)
 
-    // vamos a emitir la posicion del circulo
-    socket.emit("circle-position", position)
+    console.log("se envia el evento al servidor")
+    //ahora para manejar estos eventos que se quedan en buffer en las desconexiones y evitar que se manden despues todos los eventos al mismo tiempo al servidor ponemos la palabra volatile
+    // entonces si no hay conexion el evento no se va a mandar
+    socket.volatile.emit("circle-position", position)
 }
 
-// creamos una funcion drawCircle
 const drawCircle = position => {
     circle.style.top = position.top;
     circle.style.left = position.left;
 }
 
-// queremos mover el circle
 const circle = document.querySelector("#circle");
 document.addEventListener("mousedown", e => {
     document.addEventListener("mousemove", drag)
@@ -29,8 +28,6 @@ document.addEventListener("mouseup", e => {
     document.removeEventListener("mousemove", drag)
 })
 
-// ahora vamos a escuchar el evento move-circle que va a enviar el servidor a todos los lientes, para que todos los clientes en tiempo real sepan que pasa con la posicion del circulo
 socket.on("move-circle", position => {
-    // llamar ahora a nuestra funcion drawCircle
     drawCircle(position)
 })
