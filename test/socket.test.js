c7446f898135f8bf6feecbd218b02c5fe85b869f
Jorge Munera <gerjo9211@hotmail.com>
2025-05-11 09:44:16 -0500

    probando algunas cosas

--- test/socket.test.js
+++ test/socket.test.js
@@ -1,48 +1,62 @@
-// Para hacer test dentro de socket.io tenemos que crear un server de socket.io en nuestro test
-
 const {createServer} = require("http");
 const {Server} = require("socket.io");
 
-// si nosotros queremos testear bien esto, debemos teber alguna manera de testear la parte del cliente
-// per como estamos haciendo testing en la terminal no tenemos una forma de hacerlo en el cliente (para eso instalamos de socker.io-client), ya que asi simulamos codigo en el cliente
 const Client = require("socket.io-client");
 
-// descibe para iniciar nuestro test
 describe("testing socket.io", () => {
     let io, serverSocket, clientSocket;
-    // antes de que todo el testing se ejecute vamos a evaluar que el server este creado
     beforeAll(done => {
-        //server http
         const httpServer = createServer();
-
-        // creamos el server socket.io
         io = new Server(httpServer);
 
-        // ponemos nuestro server a escuchar
         httpServer.listen(() => {
-            // vemos en que puerto esta el server
             const port = httpServer.address().port
 
-            //definimos el cliente simulado
-            // este debe conectarse a mi server http
             clientSocket = new Client(`http://localhost:${port}`)
 
             io.on("connection", socket => {
                 serverSocket = socket
             })
 
-            // cuando el liente este ya conectado entonces done
             clientSocket.on("connect", done)
         })
     })
 
-    // y despues de que nuestros test terminen de correr usamos after
     afterAll(() => {
-        // lo que queremos es cerrar mi server de web socket
         io.close();
-
-        // y l mismo con el del cliente
         clientSocket.close();
+    });
+
+    // comenzamos con los test
+    test("Test evento", done => {
+        // cuando el cliente reciba el evento saludo
+        clientSocket.on("greeting", greet => {
+            try {
+                expect(greet).toBe("Holi");
+                done();    
+            } catch (error) {
+                done(error);
+            }
+        });
+        // creamos el evento emit desde el server
+        serverSocket.emit("greeting", "Holi")
+    });
+
+    // simulamos otro test
+    test("testing callback (acknoledgements)", done => {
+        serverSocket.on("ladrar", callback => {
+            callback("guau!")
+        });
+
+        clientSocket.emit("ladrar", arg => {
+            try {
+                expect(arg).toBe("guau!");
+                done();
+            } catch (error) {
+                done(error);
+                
+            }
+        })
     })
 })
 
